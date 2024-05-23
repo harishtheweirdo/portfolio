8d9a486c60fdd3947e7129ce4385fc55902cf239
harishtheweirdo <09harishraja9c@gmail.com>
2024-05-23 15:12:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,272 @@
+$(document).ready(function(){
+
+$("#home1").click(function(){
+    $(".lst1").css({"background": "orange", "height": "3px", });
+$(".lst2").css({"background": "none", "height": "3px"});
+$(".lst3").css({"background": "none", "height": "3px"});
+$(".lst4").css({"background": "none", "height": "3px"});
+$(".lst5").css({"background": "none", "height": "3px"});
+$(".lst6").css({"background": "none", "height": "3px"});
+$(".lst7").css({"background": "none", "height": "3px"});
+$(".lst8").css({"background": "none", "height": "3px"});
+$("#home1").css({"color": "orange"});
+$("#about").css({"color": "white"});
+$("#resume").css({"color": "white"});
+$("#services").css({"color": "white"});
+$("#myblog").css({"color": "white"});
+$("#contact").css({"color": "white"});
+$("#skills").css({"color": "white"});
+$("#projects").css({"color": "white"});
+});
+
+$("#about").click(function(){
+    $(".lst8").css({"background": "orange", "height": "3px",  });
+    $(".lst2").css({"background": "none", "height": "3px"});
+    $(".lst3").css({"background": "none", "height": "3px"});
+    $(".lst4").css({"background": "none", "height": "3px"});
+    $(".lst5").css({"background": "none", "height": "3px"});
+    $(".lst6").css({"background": "none", "height": "3px"});
+    $(".lst7").css({"background": "none", "height": "3px"});
+    $(".lst1").css({"background": "none", "height": "3px"});
+    $("#about").css({"color": "orange"});
+    $("#home1").css({"color": "white"});
+$("#resume").css({"color": "white"});
+$("#services").css({"color": "white"});
+$("#myblog").css({"color": "white"});
+$("#contact").css({"color": "white"});
+$("#skills").css({"color": "white"});
+$("#projects").css({"color": "white"});
+    });
+
+
+    $("#resume").click(function(){
+        $(".lst2").css({"background": "orange", "height": "3px"});
+        $(".lst1").css({"background": "none", "height": "3px"});
+        $(".lst3").css({"background": "none", "height": "3px"});
+        $(".lst4").css({"background": "none", "height": "3px"});
+        $(".lst5").css({"background": "none", "height": "3px"});
+        $(".lst6").css({"background": "none", "height": "3px"});
+        $(".lst7").css({"background": "none", "height": "3px"});
+        $(".lst8").css({"background": "none", "height": "3px"});
+        $(".lst9").css({"background": "none", "height": "3px"});
+        $("#resume").css({"color": "orange"});
+        $("#about").css({"color": "white"});
+$("#home1").css({"color": "white"});
+$("#services").css({"color": "white"});
+$("#myblog").css({"color": "white"});
+$("#contact").css({"color": "white"});
+$("#skills").css({"color": "white"});
+$("#projects").css({"color": "white"});
+        });
+
+
+        $("#services").click(function(){
+            $(".lst3").css({"background": "orange", "height": "3px"});
+            $(".lst2").css({"background": "none", "height": "3px"});
+            $(".lst1").css({"background": "none", "height": "3px"});
+            $(".lst4").css({"background": "none", "height": "3px"});
+            $(".lst5").css({"background": "none", "height": "3px"});
+            $(".lst6").css({"background": "none", "height": "3px"});
+            $(".lst7").css({"background": "none", "height": "3px"});
+            $(".lst8").css({"background": "none", "height": "3px"});
+            $(".lst9").css({"background": "none", "height": "3px"});
+            $("#services").css({"color": "orange"});
+            $("#about").css({"color": "white"});
+$("#resume").css({"color": "white"});
+$("#home1").css({"color": "white"});
+$("#myblog").css({"color": "white"});
+$("#contact").css({"color": "white"});
+$("#skills").css({"color": "white"});
+$("#projects").css({"color": "white"});
+            });
+
+            $("#skills").click(function(){
+                $(".lst4").css({"background": "orange", "height": "3px"});
+                $(".lst2").css({"background": "none", "height": "3px"});
+                $(".lst3").css({"background": "none", "height": "3px"});
+                $(".lst1").css({"background": "none", "height": "3px"});
+                $(".lst5").css({"background": "none", "height": "3px"});
+                $(".lst6").css({"background": "none", "height": "3px"});
+                $(".lst7").css({"background": "none", "height": "3px"});
+                $(".lst8").css({"background": "none", "height": "3px"});
+                $(".lst9").css({"background": "none", "height": "3px"});
+                $("#skills").css({"color": "orange"});
+                $("#about").css({"color": "white"});
+$("#resume").css({"color": "white"});
+$("#services").css({"color": "white"});
+$("#myblog").css({"color": "white"});
+$("#contact").css({"color": "white"});
+$("#home1").css({"color": "white"});
+$("#projects").css({"color": "white"});
+                });
+
+                $("#projects").click(function(){
+                    $(".lst5").css({"background": "orange", "height": "3px"});
+                    $(".lst2").css({"background": "none", "height": "3px"});
+                    $(".lst3").css({"background": "none", "height": "3px"});
+                    $(".lst4").css({"background": "none", "height": "3px"});
+                    $(".lst1").css({"background": "none", "height": "3px"});
+                    $(".lst6").css({"background": "none", "height": "3px"});
+                    $(".lst7").css({"background": "none", "height": "3px"});
+                    $(".lst8").css({"background": "none", "height": "3px"});
+                    $(".lst9").css({"background": "none", "height": "3px"});
+                    $("#projects").css({"color": "orange"});
+                    $("#about").css({"color": "white"});
+$("#resume").css({"color": "white"});
+$("#services").css({"color": "white"});
+$("#myblog").css({"color": "white"});
+$("#contact").css({"color": "white"});
+$("#skills").css({"color": "white"});
+$("#home1").css({"color": "white"});
+                    });
+
+                    $("#myblog").click(function(){
+                        $(".lst6").css({"background": "orange", "height": "3px"});
+                        $(".lst2").css({"background": "none", "height": "3px"});
+                        $(".lst3").css({"background": "none", "height": "3px"});
+                        $(".lst4").css({"background": "none", "height": "3px"});
+                        $(".lst5").css({"background": "none", "height": "3px"});
+                        $(".lst1").css({"background": "none", "height": "3px"});
+                        $(".lst7").css({"background": "none", "height": "3px"});
+                        $(".lst8").css({"background": "none", "height": "3px"});
+                        $(".lst9").css({"background": "none", "height": "3px"});
+                        $("#myblog").css({"color": "orange"});
+                        $("#about").css({"color": "white"});
+$("#resume").css({"color": "white"});
+$("#services").css({"color": "white"});
+$("#home1").css({"color": "white"});
+$("#contact").css({"color": "white"});
+$("#skills").css({"color": "white"});
+$("#projects").css({"color": "white"});
+                        });
+
+
+                        $("#contact").click(function(){
+                            $(".lst7").css({"background": "orange", "height": "3px"});
+                            $(".lst2").css({"background": "none", "height": "3px"});
+                            $(".lst3").css({"background": "none", "height": "3px"});
+                            $(".lst4").css({"background": "none", "height": "3px"});
+                            $(".lst5").css({"background": "none", "height": "3px"});
+                            $(".lst1").css({"background": "none", "height": "3px"});
+                            $(".lst6").css({"background": "none", "height": "3px"});
+                            $(".lst8").css({"background": "none", "height": "3px"});
+                            $(".lst9").css({"background": "none", "height": "3px"});
+                            $("#contact").css({"color": "orange"});
+                            $("#about").css({"color": "white"});
+$("#resume").css({"color": "white"});
+$("#services").css({"color": "white"});
+$("#myblog").css({"color": "white"});
+$("#home1").css({"color": "white"});
+$("#skills").css({"color": "white"});
+$("#projects").css({"color": "white"});
+                            });
+});
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+     let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+       current += increment;
+       obj.textContent = current;
+       if (current == end) {
+        clearInterval(timer);
+       }
+      }, step);
+    }
+    counter("count1", 0, 250, 3000);
+    counter("count2", 0, 250, 3000);
+    counter("count3", 0, 250, 3000);
+    counter("count4", 0, 250, 3000);
+    counter("count5", 0, 250, 3000);
+   });
+
+
+
+
+
+
+
+
+   $(document).ready(function(){
+	progress_bar();
+});
+
+function progress_bar() {
+	var speed = 30;
+	var items = $('.progress_bar').find('.progress_bar_item');
+	
+    items.each(function() {
+        var item = $(this).find('.progress');
+        var itemValue = item.data('progress');
+        var i = 0;
+        var value = $(this);
+		
+        var count = setInterval(function(){
+            if(i <= itemValue) {
+                var iStr = i.toString();
+                item.css({
+                    'width': iStr+'%'
+                });
+                value.find('.item_value').html(iStr +'%');
+            }
+            else {
+                clearInterval(count);
+            }
+            i++;
+        },speed);
+    });
+}
+
+
+
+var counter = function() {
+		
+    $('#section-counter, .hero-wrap, .ftco-counter, .ftco-about').waypoint( function( direction ) {
+
+        if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
+
+            var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
+            $('.number').each(function(){
+                var $this = $(this),
+                    num = $this.data('number');
+                    console.log(num);
+                $this.animateNumber(
+                  {
+                    number: num,
+                    numberStep: comma_separator_number_step
+                  }, 7000
+                );
+            });
+            
+        }
+
+    } , { offset: '95%' } );
+
+}
+counter();
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar7");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+
+
+
+
